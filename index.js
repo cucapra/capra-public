e122f352df1d654ed9f1b96aed7568a84b8f5ecb
Adrian Sampson <adrian@radbox.org>
2017-08-10 12:02:25 -0400

    Remove an unnecessary dep

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var markdown    = require('metalsmith-markdown');
 var layouts     = require('metalsmith-layouts');
 var sass        = require('metalsmith-sass');
 var metadata    = require('metalsmith-metadata');
-var relative    = require('metalsmith-relative');
 var collections = require('metalsmith-collections');
 var filepath    = require('metalsmith-filepath');
 var inplace     = require('metalsmith-in-place');
@@ -59,7 +58,6 @@ var site = Metalsmith(__dirname)
       },
     },
   }))
-  .use(relative())
   .use(define({
     resolve: url.resolve,  // Path join helper.
   }))
